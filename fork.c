04a96e090deb9708337ee883dbc6f260efa75790
Adil Gondal <agondal@stuy.edu>
2018-11-17 17:55:07 -0500

    .

--- fork.c
+++ fork.c
@@ -9,7 +9,7 @@ int main(){
   fork();
   fork();
   if (getpid()!=id && getppid()==id ){
-    printf("I am the child with pid:%d\n\n",getpid());
+    printf("I am the child with pid:%d\nMy parent's pid is:\n",getpid(),getppid());
   }
   return 0;
 }
