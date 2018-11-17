d58aee23703256eb396cac553cac5dd833ba0c00
Adil Gondal <agondal@stuy.edu>
2018-11-17 18:47:22 -0500

    parent

--- fork.c
+++ fork.c
@@ -8,30 +8,30 @@
 
 int main(){
   int id = getpid();
-  int sTime;
-  printf("I am the parent with pid:%d\n\n",getpid());
-  fork();
-  fork();
-  int childid;
-  if (getpid()!=id && getppid()==id ){
-    childid=getpid();
-  }
-  if(getpid()==childid){
+  printf("I am the parent with pid:%d\n\n",id);
+  int a = fork();
+  if (a!=0)
+    a = fork();
+  if(a==0){
     printf("I am the child with pid:%d. My parent's pid is:%d\n",getpid(),getppid());
     int randFile = open("/dev/random", O_RDONLY);
+    int sTime;
     read(randFile, &sTime, sizeof(int));
     close(randFile);
     sTime = (abs(sTime) % 16) + 5;
     printf("Sleep time: %d\n", sTime);
     sleep(sTime);
     printf("I'm the child and I finished.\n");
+    return sTime;
   }
-  if(getpid()=id){
-    int *status;
-    waitpid(childid(), status, 0);
-    printf( " Child %d is finished. It slept for %d seconds.", childid(), sTime);
-    printf( "Parent is done and program is ending.\n\n" );
-  }
-  
+  if(a!=0){
+    int status;
+    int cid=wait(&status);
+    if(WIFEXITED(status)) {
+            int sec = WEXITSTATUS(status);
+	    printf( " Child %d is finished. It slept for %d seconds.", cid, sec);		
+        }
+    printf( "Parent is done and pr\n\n" );
+  } 
   return 0;
 }
